46a96f2efc265e8a7171da66c3daf7cf9189fc57
Jaime Mahaffey <jaimemahaffey@gmail.com>
2019-10-10 19:39:32 -0700

    Added Adorner Class

---
+++ UBL_MeshEditor/MeshPointAdjustAdorner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using UBL_MeshEditor.Annotations;
+
+namespace UBL_MeshEditor
+{
+    public class MeshPointAdjustAdorner : Adorner
+    {
+        private Button m_incButton;
+        private Button m_decButton;
+
+        public MeshPointAdjustAdorner([NotNull] UIElement adornedElement) : base(adornedElement)
+        {
+            
+        }
+
+
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+        }
+    }
+}
